b4be8d4e8a5d4c754c079934297b4a99cbf11a96
SloanGit <sloanjeanlouispro1@gmail.com>
2025-08-17 22:29:58 +0200

    Update .eleventy.cjs

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,11 +1,21 @@
-module.exports = function (eleventyConfig) {
+// .eleventy.cjs
+module.exports = function(eleventyConfig) {
+  // Laisse Netlify CMS et /static passer tels quels
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("static");
 
   return {
-    dir: { input: ".", includes: ".", layouts: ".", output: "_site" },
+    // On compile depuis la racine du repo
+    dir: {
+      input: ".",
+      includes: "_includes",   // <-- layouts et includes ici
+      layouts: "_includes",    // <-- on confirme que les layouts sont là
+      data: "src/_data",       // <-- ton site.json est déjà ici
+      output: "_site"
+    },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     templateFormats: ["njk", "md", "html"]
   };
 };
+
