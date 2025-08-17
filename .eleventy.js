d5d82874b2da3bea0ceddf818d114d408e35eb74
SloanGit <sloanjeanlouispro1@gmail.com>
2025-08-17 21:13:19 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,8 @@
 module.exports = function (eleventyConfig) {
-  // static + admin straight to /_site
   eleventyConfig.addPassthroughCopy("static");
   eleventyConfig.addPassthroughCopy("admin");
-
   return {
-    dir: {
-      input: "content",   // your markdown & njk live here
-      includes: ".",      // allow includes from project root
-      layouts: ".",       // allow layouts at project root
-      output: "_site"
-    },
+    dir: { input: "content", includes: ".", layouts: ".", output: "_site" },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     templateFormats: ["njk", "md", "html"]
