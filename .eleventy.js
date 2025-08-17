05ec7825283250714668ce3bd7f4a77b43526e73
SloanGit <sloanjeanlouispro1@gmail.com>
2025-08-17 20:37:33 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
 module.exports = function (eleventyConfig) {
+  // copie le dossier static tel quel vers _site/static
   eleventyConfig.addPassthroughCopy({ "static": "static" });
+  eleventyConfig.addPassthroughCopy("admin"); // pour Netlify CMS si présent
 
   return {
     dir: {
-      input: ".",
-      includes: ".",   // IMPORTANT – pas de _includes requis
-      output: "_site"
+      input: ".",       // on prend la racine comme dossier d'entrée
+      includes: ".",    // IMPORTANT : les layouts/fichiers inclus peuvent être à la racine
+      output: "_site"   // dossier de sortie
     },
     templateFormats: ["njk", "md", "html"],
     markdownTemplateEngine: "njk",
