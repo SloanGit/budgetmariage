d1ff3dc49543b9310f703366507813420678ca00
SloanGit <sloanjeanlouispro1@gmail.com>
2025-08-17 23:32:47 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 // .eleventy.js (CommonJS)
 const { DateTime } = require("luxon");
 
-module.exports = function(eleventyConfig) {
-  // Laisse passer ces dossiers tels quels vers _site
+module.exports = function (eleventyConfig) {
+  // Fichiers recopiés tels quels vers _site
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("static");
 
@@ -12,16 +12,30 @@ module.exports = function(eleventyConfig) {
     return DateTime.fromJSDate(d, { zone: "utc" }).toFormat(format);
   });
 
+  // Collections pour le blog
+  eleventyConfig.addCollection("posts", (collectionApi) => {
+    return collectionApi
+      .getFilteredByGlob("content/posts/*.md")
+      .sort((a, b) => b.date - a.date);
+  });
+
+  eleventyConfig.addCollection("latestPosts", (collectionApi) => {
+    return collectionApi
+      .getFilteredByGlob("content/posts/*.md")
+      .sort((a, b) => b.date - a.date)
+      .slice(0, 3);
+  });
+
   return {
     dir: {
-      input: ".",          // on lit depuis la racine du repo
+      input: ".",           // on lit depuis la racine du repo
       includes: "_includes",
       layouts: "_includes",
-      data: "src/_data",
+      data: "src/_data",    // tu as site.json ici
       output: "_site"
     },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
-    templateFormats: ["njk","md","html"]
+    templateFormats: ["njk", "md", "html"]
   };
 };
