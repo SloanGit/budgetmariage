95817919dcba55a1269a8c1d74c6094f4aa8e97c
SloanGit <sloanjeanlouispro1@gmail.com>
2025-08-17 20:31:11 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,11 @@
 module.exports = function (eleventyConfig) {
-  // Copie /static tel quel vers _site/static
   eleventyConfig.addPassthroughCopy({ "static": "static" });
 
   return {
     dir: {
-      input: ".",        // on build depuis la racine
-      includes: ".",     // pas de dossier _includes requis
-      // ⚠️ si tu n'as PAS src/_data, ne mets pas de ligne 'data'
-      output: "_site"    // dossier de sortie
+      input: ".",
+      includes: ".",   // IMPORTANT – pas de _includes requis
+      output: "_site"
     },
     templateFormats: ["njk", "md", "html"],
     markdownTemplateEngine: "njk",
