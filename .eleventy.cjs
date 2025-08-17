8318733b3b49e20fb062c5e3fe809c6602e31d12
SloanGit <sloanjeanlouispro1@gmail.com>
2025-08-17 22:08:39 +0200

    Update and rename .eleventy.js to .eleventy.cjs

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,11 +1,11 @@
-// .eleventy.js (ESM)
-export default function (eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("static");
+
   return {
     dir: { input: ".", includes: ".", layouts: ".", output: "_site" },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     templateFormats: ["njk", "md", "html"]
   };
-}
+};
